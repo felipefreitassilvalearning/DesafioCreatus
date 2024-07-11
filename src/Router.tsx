ce50f9536863e565f576b112f2c11e09a826d379
Felipe Freitas Silva <f.freitas007@edu.pucrs.br>
2024-07-11 00:00:32 -0300

    Save: React Router Tutorial

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,8 +1,34 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Root from "./pages/Root"
+import Login from "./pages/Login"
+import NavigationError from "./pages/NavigationError"
+import Contact from "./pages/Contact"
+
 function Router() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      errorElement: <NavigationError />
+    },
+    {
+      path: "login",
+      element: <Login />,
+    },
+    {
+      path: "contacts",
+      element: <Contact />,
+      children: [
+        {
+          path: ":id",
+          element: <Contact />,
+        }
+      ]
+    }
+  ])
+
   return (
-    <div>
-      <h1>Router</h1>
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ src/pages/Root.tsx
@@ -0,0 +1,7 @@
+function Root() {
+  return (
+    <a href="/login">Go to Login</a>
+  )
+}
+
+export default Root
