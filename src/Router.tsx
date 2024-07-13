3fbcf7b51a83f2d2a0e5b02b60d61fd3630caa26
Felipe Freitas Silva <f.freitas007@edu.pucrs.br>
2024-07-12 22:09:45 -0300

    Outline Probable Routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,8 +1,58 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+
 function Router() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Root</div>,
+      errorElement: <div>NavigationError</div>
+    },
+    {
+      path: "/login",
+      element: <div>Login</div>,
+      errorElement: <div>LoginError</div>
+    },
+    {
+      path: "/users",
+      element: <div>UsersWrapper</div>,
+      errorElement: <div>UsersError</div>,
+      // loader: usersLoader,
+      // action: usersAction,
+      children: [
+        {
+          errorElement: <div>UsersNavigationError</div>,
+          children: [
+            {
+              index: true,
+              element: <div>Select a user</div>,
+            },
+            // {
+            //   path: ":userId",
+            //   element: <div>UserRead</div>,
+            //   loader: userReadLoader as any,
+            //   action: userReadAction as any,
+            // },
+            // {
+            //   path: ":userId/edit",
+            //   element: <div>UserEdit</div>,
+            //   loader: userEditLoader as any,
+            //   action: userEditAction as any,
+            // },
+            // {
+            //   path: ":userId/destroy",
+            //   element: <div>UserDelete</div>,
+            //   action: userDeleteAction as any,
+            //   errorElement: <div>UserDeleteError</div>
+            // },
+          ]
+        },
+      ]
+    },
+  ])
+
   return (
-    <div>
-      <h1>Router</h1>
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
