b9291f29d126ae487abeb19f7e3a540956960741
Felipe Freitas Silva <f.freitas007@edu.pucrs.br>
2024-07-14 16:01:39 -0300

    Users Table Styling

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,5 +1,6 @@
 import { Outlet, redirect, useLoaderData, useNavigate } from "react-router-dom"
 
+import "./Users/styles.scss"
 import { authenticate } from "../api/auth"
 import { readUsers } from "../api/users"
 import { getToken } from "../tokenHelper"
@@ -26,57 +27,60 @@ function Users() {
 	const navigate = useNavigate()
 
 	return (
-		<div className="wrapper">
-			<table>
-				<thead>
-					<tr>
-						<th colSpan={3}>Usuários</th>
-						<th colSpan={2}>
-							<button onClick={() => { navigate("create") }}>
-								Adicionar Usuário
-							</button>
-						</th>
-					</tr>
-					<tr>
-						<th>Nome</th>
-						<th>Email</th>
-						<th>Nível de Acesso</th>
-						<th hidden>Editar</th>
-						<th hidden>Excluir</th>
-					</tr>
-				</thead>
-				<tbody>
-					{users.length > 0 ? users.map((user) => (
-						<tr
-							key={user.id}
-							onClick={() => { navigate(`${user.id}/profile`) }}
-							tabIndex={0}
-							style={{ cursor: "pointer" }}
-							title="Clique para ver detalhes do usuário"
-							aria-label="Clique para ver detalhes do usuário"
-						>
-							<td>{user.name}</td>
-							<td>{user.email}</td>
-							<td>{user.level}</td>
-							<td>
-								<button onClick={(e) => {
-									e.stopPropagation()
-									navigate(`${user.id}/edit`)
-								}}>
-									Editar
+		<body>
+			<div className="wrapper">
+				<table className="usersTable">
+					<thead>
+						<tr className="actionHeader">
+							<th colSpan={3}>Usuários</th>
+							<th colSpan={2}>
+								<button onClick={() => { navigate("create") }}>
+									Adicionar Usuário
 								</button>
-							</td>
-							<td><UserDelete userId={user.id} /></td>
+							</th>
 						</tr>
-					)) : (
-						<tr>
-							<td colSpan={5}>Nenhum usuário encontrado</td>
+						<tr data-spacer></tr>
+						<tr className="infoHeader">
+							<th>Nome</th>
+							<th>Email</th>
+							<th>Nível de Acesso</th>
+							<th><span hidden>Editar</span></th>
+							<th><span hidden>Excluir</span></th>
 						</tr>
-					)}
-				</tbody>
-			</table>
+					</thead>
+					<tbody>
+						{users.length > 0 ? users.map((user) => (
+							<tr
+								key={user.id}
+								onClick={() => { navigate(`${user.id}/profile`) }}
+								tabIndex={0}
+								style={{ cursor: "pointer" }}
+								title="Clique para ver detalhes do usuário"
+								aria-label="Clique para ver detalhes do usuário"
+							>
+								<td>{user.name}</td>
+								<td>{user.email}</td>
+								<td>{user.level}</td>
+								<td>
+									<button onClick={(e) => {
+										e.stopPropagation()
+										navigate(`${user.id}/edit`)
+									}}>
+										Editar
+									</button>
+								</td>
+								<td><UserDelete userId={user.id} /></td>
+							</tr>
+						)) : (
+							<tr>
+								<td colSpan={5}>Nenhum usuário encontrado</td>
+							</tr>
+						)}
+					</tbody>
+				</table>
+			</div>
 			<Outlet />
-		</div>
+		</body>
 	)
 }
 
